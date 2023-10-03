d410565ccc16a97e3ab040615a02cec7b5bf8e0e
RuthWanzila <wanzilaruth533@gmail.com>
2023-10-03 04:18:16 -0700

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/xin/python3
+def uppercase(str):
+    for x in str:
+        if ord(x) >= ord('a') and ord(x) <= ord('z'):
+            char = chr(ord(x) - 32)
+        else:
+            char = x
+        print("{:s}".format(char), end="")
+    print('')
