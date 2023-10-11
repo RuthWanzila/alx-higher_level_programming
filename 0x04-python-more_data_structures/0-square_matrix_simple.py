4e60c15047f13a3ca40d5d0e7580fd4f291e15f3
RuthWanzila <wanzilaruth533@gmail.com>
2023-10-11 02:18:48 -0700

    python 0x04

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    result_matrix = []
+    for row in matrix:
+        squared_row = map(lambda num: num**2, row)
+        result_matrix.append(list(squared_row))
+    return result_matrix
