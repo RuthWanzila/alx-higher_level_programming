1f65e1cc3031f5fc15de6ba9affa9d3dce58f059
RuthWanzila <wanzilaruth533@gmail.com>
2023-10-24 21:49:43 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+class Square:
+    """
+    This class represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square object with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
