8d3a9b27e179d7cefc3e944a0119e0b61fedacdd
RuthWanzila <wanzilaruth533@gmail.com>
2023-10-30 07:51:35 -0700

    class Rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""defines an empty class Rectangle"""
 class Rectangle:
+    """Class thet defines a rectangle
+    """
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,30 +1,66 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """
+    Represents a rectangle.
+    """
+
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
+        """
+        Initializes a rectangle instance.
+        Args:
+            width (int): Width of the rectangle (default is 0).
+            height (int): Height of the rectangle (default is 0).
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self._width
+        """
+        Retrieves the width of the rectangle.
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Sets the width of the rectangle.
+        Args:
+            value (int): The width value to set.
+        Raises:
+            TypeError: If width is not an integer.
+            ValueError: If width is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        """
+        Retrieves the height of the rectangle.
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Sets the height of the rectangle.
+        Args:
+            value (int): The height value to set.
+        Raises:
+            TypeError: If height is not an integer.
+            ValueError: If height is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
