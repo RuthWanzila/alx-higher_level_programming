e04dcb11a0c3ef63da042b26267cd9bae02a50d0
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-06 11:37:54 -0800

    geometry

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-Module for BaseGeometry
+Module for an empty class BaseGeometry.
 """
 
 
 class BaseGeometry:
     """
-    Base class for geometry-related operations.
+    Empty class for BaseGeometry.
     """
-
-    def area(self):
-        """
-        Placeholder method for calculating the area.
-        """
-        raise Exception("area() is not implemented")
+    pass
