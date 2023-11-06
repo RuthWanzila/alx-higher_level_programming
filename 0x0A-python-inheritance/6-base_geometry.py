f5571b83ef209ef63a19046c49257c968249de47
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-06 11:39:48 -0800

    update

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,47 +1,16 @@
 #!/usr/bin/python3
 """
-Module for BaseGeometry and Rectangle classes
+Module for the BaseGeometry class.
 """
 
 
 class BaseGeometry:
     """
-    Base class for geometry-related operations.
+    Empty class for BaseGeometry.
     """
 
     def area(self):
         """
-        Placeholder method for calculating the area.
+        Raises an exception with the message "area() is not implemented".
         """
         raise Exception("area() is not implemented")
-
-    def perimeter(self):
-        """
-        Placeholder method for calculating the perimeter.
-        """
-        raise Exception("perimeter() is not implemented")
-
-
-class Rectangle(BaseGeometry):
-    """
-    Class representing a rectangle.
-    """
-
-    def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with the specified width and height.
-        """
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Calculates and returns the area of the rectangle.
-        """
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """
-        Calculates and returns the perimeter of the rectangle.
-        """
-        return 2 * (self.__width + self.__height)
