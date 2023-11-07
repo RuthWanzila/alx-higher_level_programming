57ac2e3aed797cd102e76ada15c845ba7de070ba
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-07 09:11:26 -0800

    12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,7 +11,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a Student instance with the given first name, last name, and age.
+        Initializes a Student instance with given first name, last name, age.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -20,16 +20,19 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attribute names contained in this list are retrieved.
+        If attrs is a list of strings, only attribute names
+        contained in this list are retrieved.
         Otherwise, all attributes are retrieved.
         """
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                for attr in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
         return self.__dict__
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance based on a provided dictionary.
+        Replaces attributes of Student instance based on a provided dictionary.
         """
         for key, value in json.items():
             setattr(self, key, value)
