582f224aa647e6f7aa3e4ab614e5364af31b30e9
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-07 09:14:58 -0800

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+append_after
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text into a file
+    after each line containing a specific string.
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
