f44641bdbdc92ef083a973555049e7fde111095c
RuthWanzila <wanzilaruth533@gmail.com>
2023-11-07 02:44:22 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,16 +4,10 @@ Module for read_file, function that reads
 a text file (UTF8) and prints it to stdout
 """
 
-
 def read_file(filename=""):
-    """
-    read_file: read a file using
-    with statement
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read(), end="")
-
-
-if __name__ == "__main__":
-    file_path = 'example.txt'  # Replace with the path to your file
-    read_file(file_path)
+"""
+read_file: read a file using
+with statement
+"""
+with open(filename, 'r', encoding="utf-8") as f:
+print(f.read(), end="")
