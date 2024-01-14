9156f407d67105a0c758ce57bc26f33331580a2b
RuthWanzila <wanzilaruth533@gmail.com>
2024-01-14 06:07:22 -0800

    kcj

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,47 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
-import mysql.connector
+
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    db = mysql.connector.connect(
+def main():
+    # Get MySQL credentials and state name to search from command-line arguments
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
         host="localhost",
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3],
-        port=3306
+        port=3306,
+        user=mysql_user,
+        passwd=mysql_password,
+        db=database_name
     )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (sys.argv[4],))
-    rows = cur.fetchall()
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Prepare the SQL query with the user input
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Print the selected states
     for row in rows:
         print(row)
-    cur.close()
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
