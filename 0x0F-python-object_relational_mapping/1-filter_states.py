3529aae2f38c3997687262db871dfd0681f57082
RuthWanzila <wanzilaruth533@gmail.com>
2024-01-14 05:43:59 -0800

    sql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
-
-"""
-This script lists all states with a name starting with 'N' (uppercase) from the 'hbtn_0e_0_usa' database.
-"""
-
-import sys
+"""  lists all states starting with N from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
-# Get MySQL credentials from command-line arguments
-mysql_user = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
 
-# Connect to the MySQL database
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=mysql_user,
-    passwd=mysql_password,
-    db=database_name
-)
-
-# Create a cursor object to interact with the database
-cursor = db.cursor()
-
-# Execute the SQL query to select states starting with 'N'
-query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-cursor.execute(query)
-
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
-
-# Print the selected states
-for row in rows:
-    print(row)
-
-# Close the cursor and database connection
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
