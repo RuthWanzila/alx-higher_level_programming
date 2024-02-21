5272804f2dc81f30a7378b8a9755d516b89e951d
RuthWanzila <wanzilaruth533@gmail.com>
2024-02-21 10:56:33 +0300

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', (err)=> {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("File written successfully.');
+	}
+});
+
