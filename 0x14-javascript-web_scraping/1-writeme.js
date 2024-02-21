efdad011d0032f72d63e2deb8758fd9c5f9e80c3
RuthWanzila <wanzilaruth533@gmail.com>
2024-02-21 00:24:20 -0800

    3-starwars_title.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.arg[2], process.argv[3], err => {
-	        if (err) {
-			                console.log(err);
-			        }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
